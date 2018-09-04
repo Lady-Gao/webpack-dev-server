f44d718f1893407d03e1c295e12d78c4573b5732
gaoyanan <1440374646@qq.com>
2018-09-04 14:43:25 +0800

    1

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlwebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  mode: 'development',
+  entry: "./src/index.js",
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+      test: /.jsx?$/,
+      include: [
+        path.resolve(__dirname, 'src')
+      ],
+      exclude: [
+        path.resolve(__dirname, 'node_modules'),
+        path.resolve(__dirname, 'bower_components')
+      ],
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['.json', '.js', '.jsx', '.css']
+  },
+  devtool: 'source-map',
+  devServer: {
+    historyApiFallback: true,
+    inline: true
+  },
+  plugins:[
+    new HtmlwebpackPlugin(
+      {template:'index.html'}
+    )
+  ]
+};
